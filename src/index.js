3ae65078b3e0179b32f47b7f8a7e26a21e62c0e4
Mbformation <immvp@hotmail.fr>
2022-12-14 13:13:45 +0100

    modifications noms du reste des fichiers

--- src/index.js
+++ src/index.js
@@ -15,3 +15,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// dossiers: git rm -rf --cached layouts  fichiers: git mv layouts Layouts    
